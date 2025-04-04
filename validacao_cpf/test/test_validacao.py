978e8dc408205a4668e27ec1d88e040b32eaf50b
nayarasn <santosnay99@gmail.com>
2025-04-04 19:40:03 -0300

    Criação da test_validacao.py

---
+++ validacao_cpf/test/test_validacao.py
@@ -0,0 +1,26 @@
+import unittest
+from app.validacao import validar_cpf
+
+class TestValidarCPF(unittest.TestCase):
+
+    def test_cpf_valido(self):
+        self.assertTrue(validar_cpf("123.456.789-09"))
+        self.assertTrue(validar_cpf("03723044806"))
+        self.assertTrue(validar_cpf("708.251.780-30"))
+
+    def test_cpf_invalido_formato(self):
+        self.assertFalse(validar_cpf("1234567890"))  
+        self.assertFalse(validar_cpf("123.456.789"))   
+        self.assertFalse(validar_cpf("abc.def.ghi-jk")) 
+
+    def test_cpf_invalido_digitos(self):
+        self.assertFalse(validar_cpf("111.111.111-11")) 
+        self.assertFalse(validar_cpf("12345678910")) 
+        self.assertFalse(validar_cpf("00000000000"))
+
+    def test_cpf_com_zeros_a_esquerda(self):
+        self.assertTrue(validar_cpf("000.000.000-00"))
+        self.assertTrue(validar_cpf("012.345.678-90"))
+
+if __name__ == '__main__':
+    unittest.main()
